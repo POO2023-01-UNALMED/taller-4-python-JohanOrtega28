4d1789d243fd92b74eca56e7f903f8379aa83064
JohanOrtega28 <125934568+JohanOrtega28@users.noreply.github.com>
2023-03-06 22:15:56 -0500

    Taller4Python

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,35 +1,25 @@
-
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
-
 
 class Grupo:
+    grado = "Grado 12"
 
-    grado = None
-
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None, grado = "Grado 12"):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignatruras = asignaturas
-        self.listadoAlumnos = estudiantes
-        self.grado = grado
+        self._asignaturas = [] if not asignaturas else asignaturas
+        self.listadoAlumnos = [] if not estudiantes else estudiantes
 
     def listadoAsignaturas(self, **kwargs):
-        if self._asignatruras == None:
-            self._asignatruras = []
         for x in kwargs.values():
-            self._asignatruras.append(Asignatura(x))
+            self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=None):
-        if self.listadoAlumnos == None:
-            self.listadoAlumnos = []
-        if lista == None:
-            lista = []
+        if (not lista): lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    @classmethod
+    @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
-    
-    def __str__(self) -> str:
+
+    def __str__(self):        
         return "Grupo de estudiantes: " + self._grupo
